b07dcff2f140ddab834c72224f6e7265cd12a7d9
Oscar Saul <69000320+oscarmmv@users.noreply.github.com>
2021-11-09 20:25:03 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const scrollToHome = () => {
+    document.getElementById("Home").scrollIntoView(true);
+}
+const scrollToAbout = () => {
+    document.getElementById("About").scrollIntoView(true);
+}
+const scrollToSkill = () => {
+    document.getElementById("Skill").scrollIntoView(true);
+}
+const scrollToPortfolio = () => {
+    document.getElementById("Portfolio").scrollIntoView(true);
+}
+const scrollToContact = () => {
+    document.getElementById("Contact").scrollIntoView(true);
+}
