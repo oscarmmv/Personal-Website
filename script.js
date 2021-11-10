a8e66c2c670a5430fc2bcd884ef49a9332285d1e
Oscar Saul <69000320+oscarmmv@users.noreply.github.com>
2021-11-09 22:18:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,40 @@ const scrollToPortfolio = () => {
 const scrollToContact = () => {
     document.getElementById("Contact").scrollIntoView(true);
 }
+
+window.addEventListener("scroll", function(event) {
+    var scroll = this.scrollY;
+    var activeHome = document.getElementById("nav-home");
+    var activeAbout = document.getElementById("nav-about");
+    var activeSkill = document.getElementById("nav-skill");
+    var activePortfolio = document.getElementById("nav-portfolio");
+    var activeContact = document.getElementById("nav-contact");
+    console.log(scroll);
+    if(scroll >= 0 && scroll <= 958) {
+        activeHome.classList.add("active-state");
+    } else {
+        activeHome.classList.remove("active-state");
+    }
+    if(scroll >= 959 && scroll <= 1600) {
+        activeAbout.classList.add("active-state");
+    } else {
+        activeAbout.classList.remove("active-state");
+    }
+    if(scroll >= 1601 && scroll <= 2600) {
+        activeSkill.classList.add("active-state");
+    } else {
+        activeSkill.classList.remove("active-state");
+    }
+    if(scroll >= 2601 && scroll <= 3600) {
+        activePortfolio.classList.add("active-state");
+    } else {
+        activePortfolio.classList.remove("active-state");
+    }
+    if(scroll > 3600) {
+        activeContact.classList.add("active-state");
+    } else {
+        activeContact.classList.remove("active-state");
+    }
+
+});
+
