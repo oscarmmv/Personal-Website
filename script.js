de41020a10608f5b7975229ffc7d9c6d2c315bcc
Oscar Saul <oscarsaul.mmv@gmail.com>
2022-03-15 22:26:04 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const swup = new Swup(); // only this line when included with script tag
+
+
+$(function() {
+    AOS.init();
+  });
+
+
+// function home() {
+//   document.getElementById("splash-page").style.display = "block"; 
+//   document.getElementById("splash-page").style.visibility = "visible"; 
+//   document.getElementById("home-page").style.display = "block"; 
+//   document.getElementById("home-page").style.visibility = "visible";  
+
+//   document.getElementById("works-page").style.display = "none"; 
+//   document.getElementById("works-page").style.visibility = "hidden"; 
+//   console.log(document.getElementById("works")[0].getAttribute("href"));
+// }
+// function works() {  
+//   document.getElementById("home-page").style.display = "none"; 
+//   document.getElementById("home-page").style.visibility = "hidden";   
+
+//   document.getElementById("works-page").style.display = "block"; 
+//   document.getElementById("works-page").style.visibility = "visible";   
+// }
