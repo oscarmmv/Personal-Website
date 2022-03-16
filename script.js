8901b1930f3535feb8b28dc77abb4f0810c3ca17
Oscar Saul <oscarsaul.mmv@gmail.com>
2022-03-15 22:40:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
-const swup = new Swup(); // only this line when included with script tag
+const swup = new Swup(); 
 
 
 $(function() {
     AOS.init();
-  });
+});
 
 
-// function home() {
-//   document.getElementById("splash-page").style.display = "block"; 
-//   document.getElementById("splash-page").style.visibility = "visible"; 
-//   document.getElementById("home-page").style.display = "block"; 
-//   document.getElementById("home-page").style.visibility = "visible";  
-
-//   document.getElementById("works-page").style.display = "none"; 
-//   document.getElementById("works-page").style.visibility = "hidden"; 
-//   console.log(document.getElementById("works")[0].getAttribute("href"));
-// }
-// function works() {  
-//   document.getElementById("home-page").style.display = "none"; 
-//   document.getElementById("home-page").style.visibility = "hidden";   
-
-//   document.getElementById("works-page").style.display = "block"; 
-//   document.getElementById("works-page").style.visibility = "visible";   
-// }
