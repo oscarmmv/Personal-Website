43d9b8826f76bbb57321c0f5f6e7e4dec4aa2fd9
Oscar Saul <oscarsaul.mmv@gmail.com>
2022-03-24 16:20:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $(function() {
     AOS.init();
 });
 
-if(localStorage.getItem("theme") == "dark") {
+function setLightTheme() { 
   document.documentElement.style.setProperty('--main-color', '#e2e2e2');
   document.documentElement.style.setProperty('--main-text-color', '#000');
   document.documentElement.style.setProperty('--secondary-text-color', '#e7e7e7');
@@ -18,7 +18,9 @@ if(localStorage.getItem("theme") == "dark") {
   document.documentElement.style.setProperty('--theme-color', '#121212');
   document.getElementById("about-logo").src = "https://i.ibb.co/n8cPY4w/light-logo.jpg"; 
   document.getElementById("nav-logo").src = "https://i.ibb.co/n8cPY4w/light-logo.jpg"; 
-} else {
+}
+
+function setDarkTheme() {
   document.documentElement.style.setProperty('--main-color', '#121212');
   document.documentElement.style.setProperty('--main-text-color', '#fff');
   document.documentElement.style.setProperty('--secondary-text-color', '#202023');
@@ -28,25 +30,19 @@ if(localStorage.getItem("theme") == "dark") {
   document.getElementById("nav-logo").src = "https://i.ibb.co/b17jVHF/logo.jpg"; 
 }
 
+if(localStorage.getItem("theme") == "dark") {
+  setLightTheme();
+} else {
+  setDarkTheme();
+}
+
 function themeMode() {
   if(localStorage.getItem("theme") == "light") {
     localStorage.setItem("theme", "dark");
-    document.documentElement.style.setProperty('--main-color', '#e2e2e2');
-    document.documentElement.style.setProperty('--main-text-color', '#000');
-    document.documentElement.style.setProperty('--secondary-text-color', '#e7e7e7');
-    document.documentElement.style.setProperty('--secondary-color', '#e7e7e7');
-    document.documentElement.style.setProperty('--theme-color', '#121212');
-    document.getElementById("about-logo").src = "https://i.ibb.co/n8cPY4w/light-logo.jpg"; 
-    document.getElementById("nav-logo").src = "https://i.ibb.co/n8cPY4w/light-logo.jpg"; 
+    setLightTheme(); 
   } else {
     localStorage.setItem("theme", "light");
-    document.documentElement.style.setProperty('--main-color', '#121212');
-    document.documentElement.style.setProperty('--main-text-color', '#fff');
-    document.documentElement.style.setProperty('--secondary-text-color', '#202023');
-    document.documentElement.style.setProperty('--secondary-color', '#202023');
-    document.documentElement.style.setProperty('--theme-color', '#fff');
-    document.getElementById("about-logo").src = "https://i.ibb.co/b17jVHF/logo.jpg"; 
-    document.getElementById("nav-logo").src = "https://i.ibb.co/b17jVHF/logo.jpg"; 
+    setDarkTheme(); 
   }
 }
 
